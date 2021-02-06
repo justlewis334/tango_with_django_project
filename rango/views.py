933ab8375aecbc1836b70c6cca65e06583f02556
Lewis Haggerty <2472305H@student.gla.ac.uk>
2021-02-06 13:19:13 +0000

    Implemented login functionality ch9

--- rango/views.py
+++ rango/views.py
@@ -7,6 +7,8 @@ from django.shortcuts import redirect
 from rango.forms import PageForm
 from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
 
 def index(request):
     # Query the database for a list of ALL categories currently stored.
@@ -137,3 +139,23 @@ def register(request):
                   context = {'user_form': user_form,
                              'profile_form': profile_form,
                              'registered': registered})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('rango:index'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied")
+    else:
+        return render(request, 'rango/login.html')
